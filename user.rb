57fa3519777cf1d696d4927d6a457082f9fb9d9a
jose-gomes-junior <jose.gomesjunior@telus.com>
2022-09-17 17:24:58 -0600

    Refinement - Simple example

--- user.rb
+++ user.rb
@@ -1,7 +1,17 @@
+module CHR
+  refine Object do
+    def blank?
+      nil? || empty?
+    end
+  end
+end
+
+using CHR
+
 class User
   attr_accessor :first_name, :middle_name, :last_name
 
   def full_name
-    [first_name, middle_name, last_name].reject(&:nil?).reject(&:empty?).map(&:capitalize).join(' ')
+    [first_name, middle_name, last_name].reject(&:blank?).map(&:capitalize).join(' ')
   end
 end
