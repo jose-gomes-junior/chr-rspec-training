b2f6260b820fd1103412f554298222ed734357a4
jose-gomes-junior <jose.gomesjunior@telus.com>
2022-09-17 16:54:41 -0600

    Fix full_name when middle_name is nil

--- user.rb
+++ user.rb
@@ -2,6 +2,6 @@ class User
   attr_accessor :first_name, :middle_name, :last_name
 
   def full_name
-    "#{first_name} #{middle_name} #{last_name}"
+    [first_name, middle_name, last_name].reject(&:nil?).join(' ')
   end
 end
